4441c17cab8cc192195f8c52d60945a48a3e3779
fabio-roberto <fabiorobertosti@gmail.com>
2024-06-05 09:40:22 -0300

    Criação das classes

--- loja_virtual_mentoria/src/main/java/com/fabio/lojavirtual/model/CategoriaProduto.java
+++ loja_virtual_mentoria/src/main/java/com/fabio/lojavirtual/model/CategoriaProduto.java
@@ -16,6 +16,9 @@ import javax.persistence.Table;
 @SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
 public class CategoriaProduto implements Serializable {
 
+	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
