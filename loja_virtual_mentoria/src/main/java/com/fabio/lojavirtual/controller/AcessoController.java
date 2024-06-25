463ff5b5c63cc203bc6892fdc4b34b05da42f63a
fabio-roberto <fabiorobertosti@gmail.com>
2024-06-25 16:46:49 -0300

    Finalizando a classe de teste

--- loja_virtual_mentoria/src/main/java/com/fabio/lojavirtual/controller/AcessoController.java
+++ loja_virtual_mentoria/src/main/java/com/fabio/lojavirtual/controller/AcessoController.java
@@ -1,9 +1,14 @@
 package com.fabio.lojavirtual.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +26,8 @@ public class AcessoController {
 	private AcessoService acessoService;
 	
 	@Autowired
-	private AcessoRepository acessoRepository;
+	private AcessoRepository acessoRepository;	
+	
 	
 	@ResponseBody /*Poder dar um retorno da API*/
 	@PostMapping(value = "**/salvarAcesso") /*Mapeando a url para receber um JSON*/
@@ -37,6 +43,30 @@ public class AcessoController {
 		
 		acessoRepository.deleteById(acesso.getId());
 		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
+	}
+	
+	@ResponseBody 
+	@DeleteMapping(value = "**/deleteAcessoPorId/{id}") 
+	public ResponseEntity<?> deleteAcessoPorId(@PathVariable("id") Long id) {
+		
+		acessoRepository.deleteById(id);
+		return new ResponseEntity("Acesso Removido",HttpStatus.OK);
+	}
+	
+	@ResponseBody 
+	@GetMapping(value = "**/obterAcesso/{id}") 
+	public ResponseEntity<Acesso> obterAcesso(@PathVariable("id") Long id) {
+		
+		Acesso acesso = acessoRepository.findById(id).get();
+		return new ResponseEntity<Acesso>(acesso,HttpStatus.OK);
+	}
+	
+	@ResponseBody 
+	@GetMapping(value = "**/buscarPorDescricao/{desc}") 
+	public ResponseEntity<List<Acesso>> buscarPorDescricao(@PathVariable("desc") String desc) {
+		
+		List<Acesso> acesso = acessoRepository.buscarAcessoDesc(desc);
+		return new ResponseEntity<List<Acesso>>(acesso,HttpStatus.OK);
 	}
 
 }
